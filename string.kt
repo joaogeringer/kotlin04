9dbb785cef620df6514d0bea2b026378c5ff3367
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2023-04-25 10:37:48 -0300

    Create string.kt

---
+++ string.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    val nome =  "Tony"
+    val sobrenome = "Stark"
+
+    println("Olá, $nome $sobrenome")
+    println("Double: ${Double.MAX_VALUE}")
+
+    var str = "asdasdasdadasdasdaddas" +
+            "fsdasdasdasdadasda" +
+            "sdjbaisdnalksmdasdas" +
+            "dasaiojdnmasçldmaisunsopik"
+
+}
